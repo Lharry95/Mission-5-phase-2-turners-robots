3d66d6f5160b622228f874510b30ebe1842e3dea
Justin <justin.uaisele@gmail.com>
2026-03-21 15:45:48 +1300

    feat: add sample listing data for product listing page

---
+++ server/data/listings.js
@@ -0,0 +1,11 @@
+const listings = [
+  {
+    id: "1",
+    title: "PlayStation 5 Console",
+    image:
+      "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?auto=format&fit=crop&w=800&q=80",
+    currentBid: 650,
+  },
+];
+
+module.exports = listings;
